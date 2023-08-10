3e3e6a3576bc22fbc848d250843ed73658f2ee71
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-08-10 11:53:07 -0400

    authrntication set upaaa'

--- honeyrae/urls.py
+++ honeyrae/urls.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from repairsapi.views import register_user, login_user
+from rest_framework import routers
+from repairsapi.views import CustomerView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'customers', CustomerView, 'customer')
 
 urlpatterns = [
     # Requests to http://localhost:8000/register will be routed to the register_user function
-path('register', register_user)
-# Requests to http://localhost:8000/login will be routed to the login_user function
-path('login', login_user)
+    path('register', register_user),
+    # Requests to http://localhost:8000/login will be routed to the login_user function
+    path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
