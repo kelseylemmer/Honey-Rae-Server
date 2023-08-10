e02800d19c2103642d57711f9a09ba6d62b7c5db
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-08-10 15:23:15 -0400

    ticket and employee views

--- honeyrae/urls.py
+++ honeyrae/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import include
 from django.urls import path
 from repairsapi.views import register_user, login_user
 from rest_framework import routers
-from repairsapi.views import CustomerView
+from repairsapi.views import CustomerView, EmployeeView, TicketView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'customers', CustomerView, 'customer')
+router.register(r'employees', EmployeeView, 'employee')
+router.register(r'tickets', TicketView, 'ticket')
 
 urlpatterns = [
     # Requests to http://localhost:8000/register will be routed to the register_user function
